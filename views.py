35a2688d5819730558927dc778ddee04d5c17459
Andre_Magalhaes <andre.ukent@gmail.com>
2016-05-10 11:13:38 +0100

    Bootstrap added

--- views.py
+++ views.py
@@ -19,8 +19,11 @@ app = Flask(__name__)
 app.config.from_object('_config')
 db = SQLAlchemy(app)
 
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 10
+
 # import models (dd tables)
 from models import Task, User
+
 
 ####################
 # helper functions #
@@ -35,6 +38,25 @@ def login_required(test):
 			flash('You need to login first.')
 			return redirect(url_for('login')) 
 	return wrap
+
+
+def flash_errors(form):
+	for field, errors in form.errors.items():
+		for error in errors:
+			flash(u"Error in the %s field - %s" % (getattr(form, field).label.text, error), 'error')
+
+
+
+def open_tasks(): 
+	return db.session.query(Task) .filter_by(status='1') \
+	.order_by(Task.due_date.asc())
+
+
+def closed_tasks(): 
+	return db.session.query(Task) .filter_by(status='0') \
+	.order_by(Task.due_date.asc())
+
+
 
 
 
@@ -84,8 +106,6 @@ def register():
 			db.session.commit()
 			flash('Thanks for registering. Please login.') 
 			return redirect(url_for('login'))
-		else:
-			error = 'Incorrect data input. Please try again!'
 
 	return render_template('register.html', form=form, error=error)
 
@@ -96,17 +116,11 @@ def register():
 @app.route('/tasks/') 
 @login_required
 def tasks():
-	open_tasks = db.session.query(Task) .filter_by(status='1') \
-	.order_by(Task.due_date.asc())
-
-	closed_tasks = db.session.query(Task) .filter_by(status='0') \
-	.order_by(Task.due_date.asc())
-
 	return render_template(
 		'tasks.html', 
 		form=AddTaskForm(request.form), 
-		open_tasks=open_tasks, 
-		closed_tasks=closed_tasks
+		open_tasks=open_tasks(), 
+		closed_tasks=closed_tasks()
 	)
 
 
@@ -114,7 +128,7 @@ def tasks():
 @app.route('/add/', methods=['POST']) 
 @login_required
 def new_task():
-	
+	error = None
 	form = AddTaskForm(request.form)
 
 	if request.method == 'POST':
@@ -132,12 +146,13 @@ def new_task():
 			db.session.commit()
 
 			# flash message and redirect to tasks.html
-			flash('New entry was successfully posted. Thanks.') 
+			flash('New entry was successfully posted. Thanks.')
+			return redirect(url_for('tasks')) 
 
-		else:
-			error = 'Incorrect data input. Please try again!'
+	return render_template('tasks.html', form=form, error=error,
+		open_tasks=open_tasks(), closed_tasks=closed_tasks())
 	
-	return redirect(url_for('tasks'))
+	
 
 
 # Mark tasks as complete
